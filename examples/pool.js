82abd4787ec9c79da2b7ef85e741837febb3be1d
Abdirahim Musse <abdirahim.musse@ibm.com>
2018-05-24 16:40:08 -0500

    Added example for DBPool without using aggregates

---
+++ examples/pool.js
@@ -0,0 +1,29 @@
+//adjust require path as needed
+const {DBPool} = require('../lib/idb-pconnector');
+//set the debug to true to view verbose output call
+const pool = new DBPool({}, {debug: true});
+//remember to use await you must wrap within async Function.
+async function poolExample(){
+
+  //attach() returns an available connection from the pool.
+  let connection = pool.attach(),
+    results = null;
+    
+  try {
+    await connection.getStatement().prepare("CALL QIWS.GET_MEMBERS('QIWS','QCUSTCDT')");
+    await connection.getStatement().execute();
+    results = await connection.getStatement().fetchAll();
+    
+    if (results !== null){
+      console.log(`\n\nResults: \n${JSON.stringify(results)}`);
+    }
+    //closes statments makes the Connection available for reuse.
+    await pool.detach(connection);
+    
+  } catch (err){
+    console.log(`Error was: \n\n${err.stack}`);
+    pool.retire(connection);
+  }
+};
+    
+poolExample();
