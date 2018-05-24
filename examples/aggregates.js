6dd62f3c7f1233628f95143a0a82258b49a92c9f
Abdirahim Musse <abdirahim.musse@ibm.com>
2018-05-24 16:40:53 -0500

    Updated aggregates example with try-catch

--- examples/aggregates.js
+++ examples/aggregates.js
@@ -3,28 +3,41 @@ const {DBPool} = require('../lib/idb-pconnector');
 //set the debug to true to view verbose output call
 const pool = new DBPool({}, {debug: true});
 //remember to use await you must wrap within async Function.
-async function exampleRun(){
+async function aggregatesRun(){
   //Prepare and execute an SQL statement.
-  console.log('\nPrepare and Execute\n');
-  let results = await pool.prepareExecute("CALL QIWS.GET_MEMBERS('QIWS','QCUSTCDT')");
-  if (results !== null) {console.log(`\n\n${JSON.stringify(results)}\n\n`);}
-  /*
+  try {
+    console.log('\nPrepare and Execute\n');
+    let results = await pool.prepareExecute("CALL QIWS.GET_MEMBERS('QIWS','QCUSTCDT')");
+  
+    if (results !== null) {
+      console.log(`\n\n${JSON.stringify(results)}\n\n`);
+    }
+    /*
     Params are passed as an array values.
     The order of the params indexed in the array should map to the order of the parameter markers(i.e. '?').
   */
-  console.log('\nPrepare Bind & Execute\n');
-  let sqlBind = 'INSERT INTO QIWS.QCUSTCDT VALUES (?,?,?,?,?,?,?,?,?,?,?) with NONE';
-  let params = [4949, 'Johnson', 'T J', '452 Broadway', 'MSP', 'MN', 9810, 2000, 1, 250, 0.00];
-  let results2 = await pool.prepareExecute(sqlBind, params);
-  if (results2 !== null) {console.log(`\n\n${JSON.stringify(results2)}\n\n`);}
-  /*
+    console.log('\nPrepare Bind & Execute\n');
+    let sql = 'INSERT INTO QIWS.QCUSTCDT VALUES (?,?,?,?,?,?,?,?,?,?,?) with NONE',
+      params = [4949, 'Johnson', 'T J', '452 Broadway', 'MSP', 'MN', 9810, 2000, 1, 250, 0.00],
+      results2 = await pool.prepareExecute(sql, params);
+
+    if (results2 !== null){
+      console.log(`\n\n${JSON.stringify(results2)}\n\n`);
+    }
+    /*
     Quickly execute a statement by providing the SQL to the runSql() function
     NOTE: Stored Procedures should use the prepareExecute() method instead.
   */
-  console.log('\nRun a Query\n');
-  let results3 = await pool.runSql(`SELECT * FROM QIWS.QCUSTCDT WHERE CUSNUM = ${params[0]}`);
-  if (results3 !== null) {console.log(`\n${JSON.stringify(results3)}`);}
-  console.log('\nDone');
-};
+    console.log('\nRun a Query\n');
+    let results3 = await pool.runSql(`SELECT * FROM QIWS.QCUSTCDT WHERE CUSNUM = ${params[0]}`);
+  
+    if (results3 !== null) {
+      console.log(`\n${JSON.stringify(results3)}`);
+    }
+    console.log('\nDone');
 
-exampleRun();
+  } catch (err){
+    console.log(`Error was: ${err.stack}`);
+  }
+}
+aggregatesRun();
