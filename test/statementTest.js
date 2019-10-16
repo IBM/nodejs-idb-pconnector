2ca9eeddf1180ba686af6f2c0230eeb36babc385
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2019-10-16 13:22:03 -0500

    test: for enableNumericTypeConversion() #54

--- test/statementTest.js
+++ test/statementTest.js
@@ -473,6 +473,57 @@ describe('Statement Class Tests', () => {
     });
   });
 
+  describe('enableNumericTypeConversion', () => {
+    it('should default to false', async () => {
+      const statement = new Statement();
+      expect(statement.enableNumericTypeConversion()).to.equal(false);
+    });
+
+    it('should return numeric data as strings when false', async () => {
+      const sql = `select cast(-32768 as SMALLINT) MIN_SMALLINT,
+      cast(+32767 as SMALLINT) MAX_SMALLINT
+      from sysibm.sysdummy1`;
+
+      const statement = new Statement();
+      const result = await statement.exec(sql);
+      expect(result).to.be.an('array');
+      expect(result.length).to.be.greaterThan(0);
+      expect(result).to.eql([{ MIN_SMALLINT: '-32768', MAX_SMALLINT: '32767' }]);
+    });
+
+    it('should return numeric data as Number when safe to do so', async () => {
+      const sql = `select 
+        cast(-32768 as SMALLINT) MIN_SMALLINT,
+        cast(+32767 as SMALLINT) MAX_SMALLINT,
+        cast(-2147483648 as INT) MIN_INT,
+        cast(+2147483647 as INT) MAX_INT,
+        cast(999999999999999 as DECIMAL(15,0)) as DEC_SAFE_15_0,
+        cast(.999999999999999 as DECIMAL(15,15)) as DEC_SAFE_15_15,
+        --these values do not fit in a javascript number datatype
+        cast(-9223372036854775808 as BIGINT) MIN_BIGINT,
+        cast(+9223372036854775807 as BIGINT) MAX_BIGINT,
+        cast(9999999999999999 as DECIMAL(16,0)) as DEC_NOT_SAFE_16_0
+         from sysibm.sysdummy1`;
+
+      const statement = new Statement();
+      expect(statement.enableNumericTypeConversion(true)).to.equal(true);
+      const result = await statement.exec(sql);
+      expect(result).to.be.an('array');
+      expect(result.length).to.be.greaterThan(0);
+      expect(result).to.eql([{
+        MIN_SMALLINT: -32768,
+        MAX_SMALLINT: 32767,
+        MIN_INT: -2147483648,
+        MAX_INT: 2147483647,
+        DEC_SAFE_15_0: 999999999999999,
+        DEC_SAFE_15_15: 0.999999999999999,
+        MIN_BIGINT: '-9223372036854775808',
+        MAX_BIGINT: '9223372036854775807',
+        DEC_NOT_SAFE_16_0: '9999999999999999',
+      }]);
+    });
+  });
+
   // TODO
   describe.skip('stmtError', () => {
     it('Returns the diagnostic information ', async () => {
